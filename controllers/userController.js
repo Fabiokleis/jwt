28b6dbebc481336dfda1daaa89f94b363e2a9490
fabiokleis <fabiohkrc@gmail.com>
2021-05-05 22:50:03 -0300

    crud quase feito, falta só dar um read e update. Api pega dados via json etc

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,36 @@
-const userModel = require('../models/userModel.js');
+const User = require('../models/userModel.js');
 
 const userController = {
 
     register: async (req, res) => {
-        res.send("hello new user");
+       
+        const user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        });
+
+        try{
+            const savedUser = await user.save();
+            res.status(200).send(savedUser);
+        }catch(err){
+            res.status(400).send(err);
+        }
     },
 
     login: async (req, res) => {
         res.send("hello and sign in");
-    }
+    },
+
+    deleteUser: async (req, res) => {
+        const id = req.body.id;
+        try{
+            const deletedUser = await User.findByIdAndDelete(id);
+            res.status(200).send(deletedUser);
+        }catch(err){
+            res.status(404).send(err);
+        }
+    },
 
 }
 module.exports = userController;
--- models/userModel.js
+++ models/userModel.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    email: {type: String, required: true},
-    name: {type: String, required: true},
-    password: {type: String, required: true}
+    name: {type: String, required: true, minlength: 3, maxlength: 50},
+    email: {type: String, required: true, minlength: 5, maxlength: 50},
+    password: {type: String, required: true, minlength: 8, maxlength: 200},
+    createdAt: {type: Date, default: Date.now }
 });
 
 const userModel = mongoose.model('userModel', userSchema);
