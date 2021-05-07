5b7233096b72c86df6b53868f174fe2d7494d835
fabiokleis <fabiohkrc@gmail.com>
2021-05-07 19:28:39 -0300

    instalado o modulo Joi para fazer umas validaçoes de register e login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,20 @@
 const User = require('../models/userModel.js');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const { registerValidate, loginValidate } = require('./validate.js');
+
 
 const userController = {
 
     register: async (req, res) => {
-       
+      
+        const { error } = registerValidate(req.body);
+        if(error) return res.status(400).send(error.message);
+
+
+        const selectedUser = await User.findOne({ email: req.body.email });
+        if(selectedUser) return res.status(400).send("Email exists");
+
         const user = new User({
             name: req.body.name,
             email: req.body.email,
@@ -22,6 +31,9 @@ const userController = {
     },
 
     login: async (req, res) => {
+        const { error } = loginValidate(req.body);
+        if(error) return res.status(400).send(error.message);
+
         const email = req.body.email;
         const passwd = req.body.password;
 
